a404420dcd97bee30c6fbcb1179fc8b496e056d0
Mohssin <barcalover016@gmail.com>
2024-02-12 19:09:58 +0100

    finishing project

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,4 +1,5 @@
 import { Comment } from "../models/commentSchema.js";
+import { oneMonth } from "../utils/calculateOneMonth.js";
 import { errorHandler } from "../utils/error.js";
 
 export const createComment = async (req, res, next) => {
@@ -23,6 +24,32 @@ export const createComment = async (req, res, next) => {
 };
 
 export const getComments = async (req, res, next) => {
+  const { isAdmin } = req.user;
+  const { startIndex, limit, sort } = req.query;
+  if (!isAdmin) {
+    return next(errorHandler(401, "Unauthorized"));
+  }
+  const skipping = parseInt(startIndex) || 0;
+  const limitSize = parseInt(limit) || 10;
+  const sortDir = sort === "desc" ? 1 : -1;
+  try {
+    const comments = await Comment.find()
+      .sort({ createdAt: sortDir })
+      .skip(skipping)
+      .limit(limitSize);
+    const totalComments = await Comment.countDocuments();
+    const oneMonthAgo = oneMonth();
+    const lastMonthComments = await Comment.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    return res.status(200).json({ comments, totalComments, lastMonthComments });
+  } catch (err) {
+    console.log(err);
+    next(errorHandler(500, "Somthing went wrong"));
+  }
+};
+
+export const getCommentsByPostId = async (req, res, next) => {
   const { postId } = req.params;
   try {
     const posts = await Comment.find({ postId })
@@ -36,8 +63,25 @@ export const getComments = async (req, res, next) => {
 };
 
 export const updateComment = async (req, res, next) => {
+  const { _id: userId, isAdmin } = req.user;
   const { commentId } = req.params;
+  const { content } = req.body;
   try {
+    const comment = await Comment.findById(commentId).select("userId");
+    if (!comment) {
+      return next(errorHandler(404, "Comment not found"));
+    }
+    if (!isAdmin || comment.userId.toString() !== userId) {
+      return next(errorHandler(403, "You cannot edit this comment"));
+    }
+    const updatedComment = await Comment.findByIdAndUpdate(
+      commentId,
+      {
+        content,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedComment);
   } catch (err) {
     console.log(err);
     next(errorHandler(500, "Somthing went wrong"));
@@ -61,6 +105,25 @@ export const likeComment = async (req, res, next) => {
     await comment.save();
     await comment.populate("userId", "username profilePic -_id");
     return res.status(200).json(comment);
+  } catch (err) {
+    console.log(err);
+    next(errorHandler(500, "Somthing went wrong"));
+  }
+};
+
+export const deleteComment = async (req, res, next) => {
+  const { _id: userId, isAdmin } = req.user;
+  const { commentId } = req.params;
+  try {
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      return next(errorHandler(404, "Comment not found"));
+    }
+    if (!isAdmin || comment.userId.toString() !== userId) {
+      return next(errorHandler(403, "You cannot delete this comment"));
+    }
+    await Comment.findByIdAndDelete(commentId);
+    res.status(200).json({ message: "Comment has been deleted" });
   } catch (err) {
     console.log(err);
     next(errorHandler(500, "Somthing went wrong"));
--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -2,7 +2,9 @@ import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
 import {
   createComment,
+  deleteComment,
   getComments,
+  getCommentsByPostId,
   likeComment,
   updateComment,
 } from "../controllers/commentController.js";
@@ -10,8 +12,10 @@ import {
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
-router.get("/get-comments/:postId", getComments);
+router.get("/get-comments/:postId", getCommentsByPostId);
+router.get("/get-comments", verifyToken, getComments);
 router.put("/edit/:commentId", verifyToken, updateComment);
 router.put("/like/:commentId", verifyToken, likeComment);
+router.delete("/delete/:commentId", verifyToken, deleteComment);
 
 export default router;
